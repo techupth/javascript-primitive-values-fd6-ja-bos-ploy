f8644bc6766b0eb81c4d3a6abd6e79399c921396
boschaiyadej <boschaiyadej@gmail.com>
2024-05-09 14:18:20 +0700

    feat(ex-1-2-3-4-5-6-7-8): answer question

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrices = 27000;
+let discountPrices = (totalPrices * 25) / 100;
+console.log(discountPrices);
+// discount = 6750
